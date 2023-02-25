cad168b4f885ae78eadc90e7cca32e39d6a4608f
+John <+>
2023-02-25 11:27:40 +0300

    DONE

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,18 +1,8 @@
 class Character < ActiveRecord::Base
-  
-    belongs_to :actor
-    belongs_to :show
-
-  def actor
-    Actor.find(self.actor_id)
-  end
-
-  def show
-    Show.find(self.show_id)
-  end
+  belongs_to :actor
+  belongs_to :show
 
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
-
 end
